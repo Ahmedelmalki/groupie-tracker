e6fc821401ba4656ba87446143c3ce636fa57a3d
Ahmedelmalki <elmalkiahmed02@gmail.com>
2024-08-18 16:24:14 +0000

    second version

--- main.go
+++ main.go
@@ -9,8 +9,27 @@ import (
 	"os"
 )
 
+// Struct to hold the initial API response URLs
+type APIResponse struct {
+	Artists   string `json:"artists"`
+	Locations string `json:"locations"`
+	Dates     string `json:"dates"`
+	Relation  string `json:"relation"`
+}
+
+// Struct to represent an individual artist
+type Artist struct {
+	Id      int      `json:"id"`
+	Image   string   `json:"image"`
+	Name    string   `json:"name"`
+	Members []string `json:"members"`
+}
+
+// Struct to hold the list of artists
+type Artists []Artist
+
 func main() {
-	response, err := http.Get("http://pokeapi.co/api/v2/pokedex/kanto/")
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api")
 	if err != nil {
 		fmt.Print(err.Error())
 		os.Exit(1)
@@ -21,14 +40,32 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var responseObject Response
+	var responseObject APIResponse
+	// Unmarshal the response into the APIResponse struct
 	json.Unmarshal(responseData, &responseObject)
 
-	fmt.Println(responseObject.Name)
-	fmt.Println(len(responseObject.Pokemon))
-
-	for i := 0; i < len(responseObject.Pokemon); i++ {
-		fmt.Println(responseObject.Pokemon[i].Species.Name)
+	// Now, make an HTTP request to get the artists' data
+	artistsResponse, err := http.Get(responseObject.Artists)
+	if err != nil {
+		log.Fatal(err)
 	}
-}
+	artistsData, err := io.ReadAll(artistsResponse.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	var artists Artists
+	err = json.Unmarshal(artistsData, &artists)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Print out the artist data as an example
+	for _, artist := range artists {
+		fmt.Printf("Artist ID: %d, Name: %s, Image: %s, Members: %v\n", artist.Id, artist.Name, artist.Image, artist.Members)
+	}
+
+	fmt.Println(responseObject.Artists)
+	fmt.Println(responseObject.Locations)
+	fmt.Println(responseObject.Dates)
+	fmt.Println(responseObject.Relation)
+}
