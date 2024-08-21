14912b41d65c78b250254aa86dff005470af6723
Ahmedelmalki <elmalkiahmed02@gmail.com>
2024-08-21 15:00:53 +0000

    hadi fi hkater ms

--- index.go
+++ index.go
@@ -16,9 +16,9 @@ func handleIndex(w http.ResponseWriter, r *http.Request) {
 	var dates []DateEntry
 	var relations []RelationEntry
 	fetchData("https://groupietrackers.herokuapp.com/api/artists", &artists)
-	fetchData("https://groupietrackers.herokuapp.com/api/locations", &locations)
 	fetchData("https://groupietrackers.herokuapp.com/api/dates", &dates)
 	fetchData("https://groupietrackers.herokuapp.com/api/relation", &relations)
+	fetchData("https://groupietrackers.herokuapp.com/api/locations", &locations)
 
 	// Parse the HTML template file
 	tmpl, err := template.ParseFiles("static/index.html")
@@ -39,6 +39,68 @@ func handleIndex(w http.ResponseWriter, r *http.Request) {
 		Dates:     dates,
 		Relations: relations,
 	})
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
+
+// func handleProfile(w http.ResponseWriter, r *http.Request) {
+// 	var artists []Artist
+// 	var locations []LocationEntry
+// 	var dates []DateEntry
+// 	var relations []RelationEntry
+// 	fetchData("https://groupietrackers.herokuapp.com/api/artists", &artists)
+// 	fetchData("https://groupietrackers.herokuapp.com/api/dates", &dates)
+// 	fetchData("https://groupietrackers.herokuapp.com/api/relation", &relations)
+// 	fetchData("https://groupietrackers.herokuapp.com/api/locations", &locations)
+// 	tmpl, err := template.ParseFiles("static/profile.html")
+// 	if err != nil {
+// 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+// 		return
+// 	}
+// 	err = tmpl.ExecuteTemplate(w, "index.html", struct {
+// 		Artists   []Artist
+// 		Locations []LocationEntry
+// 		Dates     []DateEntry
+// 		Relations []RelationEntry
+// 	}{
+// 		Artists:   artists,
+// 		Locations: locations,
+// 		Dates:     dates,
+// 		Relations: relations,
+// 	})
+// 	if err != nil {
+// 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+// 		return
+// 	}
+// }
+
+// Handler for the profile page
+func handleProfile(w http.ResponseWriter, r *http.Request) {
+	// Get the artist ID from the query parameters
+	idStr := r.URL.Query().Get("id")
+	if idStr == "" {
+		http.Error(w, "Artist ID not provided", http.StatusBadRequest)
+		return
+	}
+	// Fetch the artist's data based on the ID
+	var artist Artist
+	err := fetchData("https://groupietrackers.herokuapp.com/api/artists/"+idStr, &artist)
+	if err != nil {
+		http.Error(w, "Artist not found", http.StatusNotFound)
+		return
+	}
+
+	// Parse the profile HTML template file
+	tmpl, err := template.ParseFiles("static/profile.html")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Execute the template with the artist's data
+	err = tmpl.ExecuteTemplate(w, "profile.html", artist)
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("./static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	//handle routes
 	http.HandleFunc("/", handleIndex)
-	http.HandleFunc("/profile", handleIndex)
-
-	http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/profile", handleProfile)
+	//start the server
+	fmt.Println("Server is running at http://localhost:8090")
+	err := http.ListenAndServe(":8090", nil)
+	if err != nil {
+		fmt.Println("Failed to start server:", err)
+	}
 }
