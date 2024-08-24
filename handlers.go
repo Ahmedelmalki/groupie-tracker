6258d9b6e8175a327127ce804d9756268e87009d
Ahmedelmalki <elmalkiahmed02@gmail.com>
2024-08-24 13:56:58 +0100

    create classes for profile page elements

--- handlers.go
+++ handlers.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"regexp"
 	"text/template"
@@ -13,7 +12,7 @@ type ArtistsResponse struct {
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
-		http.Error(w, "", http.StatusMethodNotAllowed)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
@@ -26,20 +25,20 @@ func handleIndex(w http.ResponseWriter, r *http.Request) {
 
 	tmpl, err := template.ParseFiles("static/index.html")
 	if err != nil {
-		http.Error(w, "", http.StatusInternalServerError)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
 	err = tmpl.ExecuteTemplate(w, "index.html", artists)
 	if err != nil {
-		http.Error(w, "", http.StatusInternalServerError)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 }
 
 func handleProfile(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
-		http.Error(w, "method not allowed", http.StatusMethodNotAllowed )
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	// Get the artist ID from the query parameters
@@ -47,53 +46,47 @@ func handleProfile(w http.ResponseWriter, r *http.Request) {
 	patren := `\d+$`
 	re := regexp.MustCompile(patren)
 	if !re.MatchString(idStr) {
-		http.Error(w, "page not found", 404)
+		http.Error(w, "page not found", http.StatusNotFound)
 		return
 	}
 	if idStr == "" {
-		http.Error(w, "method not allowed", http.StatusMethodNotAllowed )
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	var artist Artist
 	err := fetchData("https://groupietrackers.herokuapp.com/api/artists/"+idStr, &artist)
 	if err != nil {
-		fmt.Println(1)
-		http.Error(w, "", http.StatusInternalServerError)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 	err = fetchData("https://groupietrackers.herokuapp.com/api/locations/"+idStr, &artist.Locations)
 	if err != nil {
-		fmt.Println(2)
-		http.Error(w, "", http.StatusInternalServerError)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 	err = fetchData("https://groupietrackers.herokuapp.com/api/dates/"+idStr, &artist.Dates)
 	if err != nil {
-		fmt.Println(3)
-		http.Error(w, "", http.StatusInternalServerError)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 	err = fetchData("https://groupietrackers.herokuapp.com/api/relation/"+idStr, &artist.Relations)
 	if err != nil {
-		fmt.Println(4)
-		http.Error(w, "", http.StatusInternalServerError)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 	if artist.Name == "" {
-
+		http.Error(w, "page not found", http.StatusNotFound)
 		return
 	}
 	tmpl, err := template.ParseFiles("static/profile.html")
 	if err != nil {
-		http.Error(w, "", http.StatusInternalServerError)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
 	err = tmpl.ExecuteTemplate(w, "profile.html", artist)
 	if err != nil {
-		http.Error(w, "", http.StatusNotFound)
+		http.Error(w, "page not found", http.StatusNotFound)
 		return
 	}
 }
-
-
